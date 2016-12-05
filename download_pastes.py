e25b3952277cb5691d08de6a8db5acf7489988e8
James <j1459.j1459@gmail.com>
2016-12-05 16:27:26 +0800

    Seems to be working at this point

--- download_pastes.py
+++ download_pastes.py
@@ -15,7 +15,7 @@ import re
 # Libraries
 import requests
 # Local
-import config
+import config_pastebin as config
 
 
 
@@ -47,12 +47,12 @@ def download_paste(paste_id, output_dir):
     with open(item_filepath, "wb") as mf:
         mf.write(item_response.content)
 
-    logging.debug('Saved pasteID {0!r}'.format(paste_id))
+    print('Saved pasteID {0!r}'.format(paste_id))
     return True
 
 
 def download_pastes(paste_ids, output_dir):
-    logging.debug('Downloading paste_ids: {0!r}'.format(paste_ids))
+    print('Downloading paste_ids: {0!r}'.format(paste_ids))
     for paste_id in paste_ids:
         download_paste(paste_id=paste_id, output_dir=output_dir)
     return
@@ -60,13 +60,13 @@ def download_pastes(paste_ids, output_dir):
 
 def download_user_pastes(user, output_dir):
     # To save everything by a user:
-    logging.debug('Saving pastes for user: {0!r}'.format(user))
+    print('Saving pastes for user: {0!r}'.format(user))
     # Parse the listing for that user
     user_pastes = []
     for p in xrange(1,100):
         # Load the page
         user_page_url = 'http://pastebin.com/u/{0}/{1}'.format(user, p)
-        logging.debug('Loading: {0!r}'.format(user_page_url))
+        print('Loading: {0!r}'.format(user_page_url))
         user_page_request = requests.get(user_page_url)
         page = user_page_request.content
         # Parse that page
@@ -83,7 +83,7 @@ def download_user_pastes(user, output_dir):
 
     # Save the found pastes
     if len(user_pastes) == 0:
-        logging.debug('No pastes found for user: {0!r}'.format(user))
+        print('No pastes found for user: {0!r}'.format(user))
         return
     user_output_dir = os.path.join(output_dir, user)
     if not os.path.exists(user_output_dir):
@@ -91,7 +91,7 @@ def download_user_pastes(user, output_dir):
 
     download_pastes(paste_ids=user_pastes, output_dir=user_output_dir)
 
-    logging.debug('Finished saving pastes for user: {0!r}'.format(user))
+    print('Finished saving pastes for user: {0!r}'.format(user))
     return
 
 
@@ -101,10 +101,10 @@ def test_scraping_api():
     api_test_request = requests.get(api_test_url)
 
     if len(api_test_request.content) < 200:
-        logging.error('API test response was too short')
+        print('API test response was too short')
         return False# There should be 50 results provided
     if api_test_request.content[0] != '[':
-        logging.error('API test response was not in the expected format')
+        print('API test response was not in the expected format')
         return False# The response should be a JSON array
 
     if '"scrape_url":' in api_test_request.content:
@@ -117,9 +117,7 @@ def test_scraping_api():
 ##login_url = 'http://pastebin.com/api/api_login.php'
 
 
-output_dir = os.path.join('download')
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+
 
 ##paste_id = 'dzKzY6SR'
 
@@ -129,7 +127,7 @@ if not os.path.exists(output_dir):
 
 
 
-download_user_pastes(user='waterapple', output_dir=output_dir)
+
 
 
 
@@ -148,14 +146,52 @@ download_user_pastes(user='waterapple', output_dir=output_dir)
 ##'http://pastebin.com/api/api_post.php?api_dev_key=REMOVED&api_user_key={user_key}'
 
 
+def download_users_from_file(user_list_filepath, output_dir):
+    with open(user_list_filepath, "rb") as uf:
+        c = 0
+        for line in uf:
+            c += 1
+            if c % 100:
+                print('Up to users line {0}'.format(c))
+
+            user_link = line.strip()
+            user = re.search('http://pastebin.com/u/([a-zA-Z0-9-_]+)', user_link).group(1)
+            download_user_pastes(user=user, output_dir=output_dir)
+            continue
+    return
+
+
+def download_pastes_from_file(paste_list_filepath, output_dir):
+    with open(paste_list_filepath, "rb") as pf:
+        c = 0
+        for line in pf:
+            c += 1
+            if c % 100:
+                print('Up to pastes line {0}'.format(c))
+
+            paste_link = line.strip()
+            paste_id = re.search('pastebin.com/[a-zA-Z0-9]({8})', paste_link).group(1)
+            download_paste(paste_id, output_dir=output_dir)
+            continue
+    return
+
 
 def main():
-    logging.basicConfig()
+    #logging.basicConfig()
     if not test_scraping_api():
         logging.error('Pastebin scraping API unavailible.')
         return
+
     else:
-        pass
+        # Create output folder if it does not exist
+        output_dir = os.path.join('download')
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        # Perform downloads
+        download_users_from_file(user_list_filepath=config.FOUND_USERS_FILEPATH, output_dir=output_dir)
+        download_pastes_from_file(paste_list_filepath=config.FOUND_PASTES_FILEPATH, output_dir=output_dir)
+    print('Done.')
+    return
 
 
 if __name__ == '__main__':
